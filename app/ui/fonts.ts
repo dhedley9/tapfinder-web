1ce21ca2df3abfad17001b10159d638174641fd4
David Hedley <davidjhedley1@gmail.com>
2026-01-13 21:17:11 +0000

    ADD: Fallback fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,8 @@ import { Montserrat, Google_Sans } from 'next/font/google';
 
 export const montserrat = Montserrat( { subsets: ['latin'] } );
 
-export const googleSans = Google_Sans( { subsets: ['latin'] } );
+export const googleSans = Google_Sans( { 
+    subsets: ['latin'],
+    display: 'swap',
+    fallback: [ 'Helvetica', 'sans-serif'],
+} );
