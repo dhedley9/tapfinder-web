7481ba0d4ffe670896a2fa3e171b7b12df0df2bb
David Hedley <davidjhedley1@gmail.com>
2026-01-15 22:02:31 +0000

    REMOVE: Unused import

--- app/(auth)/login/2fa/enable/page.tsx
+++ app/(auth)/login/2fa/enable/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { montserrat } from '@/app/ui/fonts';
-import LoginForm from '@/app/ui/auth/login-form';
 
 export default function Page() {
     
